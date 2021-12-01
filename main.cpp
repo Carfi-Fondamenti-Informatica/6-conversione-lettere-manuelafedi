b580563b2fea15e4f0346002e7e646e2cd5b572b
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-12-01 19:58:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+    char c;
+    cin>>c;
+    if (Verifica(c) == 1){
+        cout << char (Conversione(c)) << endl;
+      
+      ] else {
+          cout << "errore" << endl;
+      }
   return 0;
 }

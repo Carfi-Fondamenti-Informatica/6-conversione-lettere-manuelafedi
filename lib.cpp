bbe02848f68a5146d32b8bf178e5d3253e48bfe0
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-12-01 19:52:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "lib.h"
+int Verifica (char c){
+    bool risultato;
+    if(c>=65 && c<=90){
+        risultato = true;
+    }
+    else if(c>=97 && c<=122){
+        risultato = true;
+    }
+    else{
+         risultato = false;
+    } return risultato;
+}
 
+int Conversione (char c){
+    int convertito = 0;
+    if(c>=65 && c<=90){
+        convertito = c+32;
+    } else {
+        convertito = c-32;
+    } return convertito;
+}
